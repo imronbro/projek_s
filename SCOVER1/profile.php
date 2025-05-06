13b20e101a0d6b7b925dec4ec616801b00a5e507
Firman <firmanparhusip65@gmail.com>
2025-05-06 11:25:47 +0700

    err

--- SCOVER1/profile.php
+++ SCOVER1/profile.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include 'koneksi.php';
-include 'logout_notification.php';
 
 // Ambil data user berdasarkan email dari session
 $email = isset($_SESSION['user_email']) ? $_SESSION['user_email'] : null;
